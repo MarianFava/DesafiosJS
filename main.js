bd31b0b0db178d979eea7057e5bdf37f1a8e77d2
MarianFava <marianaspano@gmail.com>
2021-10-01 13:39:25 -0300

    Desafio 1 Algoritmo Simple

---
+++ main.js
@@ -0,0 +1,7 @@
+//Se ingresan los valores de los productos
+let productoA = parseInt (prompt("Precio productoA:"));
+let productoB = parseInt (prompt("Precio productoB:"));
+//Se realiza la suma de los precios ingresados
+let precioTotal = productoA + productoB;
+//Se muestra en consola
+console.log(precioTotal);
