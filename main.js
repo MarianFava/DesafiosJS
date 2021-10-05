f57ec3e497cd9114444b76774203edeadd5bcef1
MarianFava <marianaspano@gmail.com>
2021-10-05 13:43:52 -0300

    Desafio2

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 //Se ingresan los valores de los productos
-let productoA = parseInt (prompt("Precio productoA:"));
-let productoB = parseInt (prompt("Precio productoB:"));
+//let productoA = parseInt (prompt("Precio productoA:"));
+//let productoB = parseInt (prompt("Precio productoB:"));
 //Se realiza la suma de los precios ingresados
-let precioTotal = productoA + productoB;
+//let precioTotal = productoA + productoB;
 //Se muestra en consola
-console.log(precioTotal);
+//console.log(precioTotal);//
+
+let edad = parseInt(prompt("Ingrese su edad"));
+
+if (edad >=18) {
+    console.log("Es mayor de edad");
+}
+else{
+    console.log("Es menor de edad");
+    alert("Prohibida la venta a menores de 18 años");
+}
